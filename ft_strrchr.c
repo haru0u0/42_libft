3a91c70e6f788713bbb1bcc8d0640c7469eeab1f
haruka <haruka.senzaki@gmail.com>
2023-12-03 16:49:24 +0000

    ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: hsenzaki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 22:01:20 by hsenzaki          #+#    #+#             */
-/*   Updated: 2023/11/23 17:06:09 by hsenzaki         ###   ########.fr       */
+/*   Updated: 2023/12/03 16:41:09 by hsenzaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,21 +14,20 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int	count;
-char *s_copy;
+	char *s_copy;
 
-s_copy =(char *)s;
+	s_copy =(char *)s;
 	count = 0;
 	while (*s_copy != '\0')
 	{
 		s_copy++;
 		count++;
 	}
-if (c == '\0')
-{
-return(s_copy + 1);
-}
-	
-	
+	if (c == '\0')
+	{
+		return((char *)s + ft_strlen(s));
+	}
+
 	while (count >= 0)
 	{
 		if (*s_copy == (c % 256))
